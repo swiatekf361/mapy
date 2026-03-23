d42cdce67fe65b8969f12dc41dd4fe2b2bd3f64a
Fabian Świątek <144426209+swiatekf361@users.noreply.github.com>
2026-03-23 13:11:20 +0100

    Add core & Update powiaty, wojewodztwa

---
+++ core/obrazki.js
@@ -0,0 +1,39 @@
+
+obrazy = {
+    wojewodztwa_flagi: {
+        "dolnośląskie": "../img/dolnoslaskie-f.svg",
+        "kujawsko-pomorskie": "../img/kujawskopomorskie-f.svg",
+        "lubelskie": "../img/lubelskie-f.svg",
+        "lubuskie": "../img/lubuskie-f.svg",
+        "łódzkie": "../img/lodzkie-f.svg",
+        "małopolskie": "../img/malopolskie-f.svg",
+        "mazowieckie": "../img/mazowieckie-f.svg",
+        "opolskie": "../img/opolskie-f.svg",
+        "podkarpackie": "../img/podkarpackie-f.svg",
+        "podlaskie": "../img/podlaskie-f.svg",
+        "pomorskie": "../img/pomorskie-f.svg",
+        "śląskie": "../img/slaskie-f.svg",
+        "świętokrzyskie": "../img/swietokrzyskie-f.svg",
+        "warmińsko-mazurskie": "../img/warminskomazurskie-f.svg",
+        "wielkopolskie": "../img/wielkopolskie-f.svg",
+        "zachodniopomorskie": "../img/zachodniopomorskie-f.svg"
+    },
+    wojewodztwa_godla: {
+        "dolnośląskie": "../img/dolnoslaskie-g.svg",
+        "kujawsko-pomorskie": "../img/kujawskopomorskie-g.svg",
+        "lubelskie": "../img/lubelskie-g.svg",
+        "lubuskie": "../img/lubuskie-g.svg",
+        "łódzkie": "../img/lodzkie-g.svg",
+        "małopolskie": "../img/malopolskie-g.svg",
+        "mazowieckie": "../img/mazowieckie-g.svg",
+        "opolskie": "../img/opolskie-g.svg",
+        "podkarpackie": "../img/podkarpackie-g.svg",
+        "podlaskie": "../img/podlaskie-g.svg",
+        "pomorskie": "../img/pomorskie-g.svg",
+        "śląskie": "../img/slaskie-g.svg",
+        "świętokrzyskie": "../img/swietokrzyskie-g.svg",
+        "warmińsko-mazurskie": "../img/warminskomazurskie-g.svg",
+        "wielkopolskie": "../img/wielkopolskie-g.svg",
+        "zachodniopomorskie": "../img/zachodniopomorskie-g.svg"
+    }
+}
---
+++ powiaty/script.js
@@ -0,0 +1,31 @@
+function pokaz(e) {
+    szukaj("")
+    try {
+        document.getElementById(nowopen).classList.remove("this");
+    } catch (e) {null}
+    console.log(e.id);
+    try {
+        infos.nazwa.innerText = dane_id[e.id][0];
+        infos.siedziba.innerText = "Siedziba władz powiatu: " + dane_id[e.id][1];
+        infos.wojewodztwo.innerHTML = "Województwo: <img id='wojewodztwo' src='" + obrazy.wojewodztwa_flagi[dane_id[e.id][3]] + "'> " + dane_id[e.id][3];
+        infos.powierzchnia.innerText = "Powierzchnia: " + dane_id[e.id][4] + " km²";
+        infos.ludnosc.innerText = "Liczba ludności: " + dane_id[e.id][5];
+        infos.gestosc.innerText = "Gęstość zaludnienia: " + dane_id[e.id][6] + " os/km²";
+        infos.tablice.innerHTML = "Tablice rejestracyjne: ";
+        for (i=0; i<dane_id[e.id][2].length; i++)
+            infos.tablice.innerHTML += "<span title='" +
+            (dane_id[e.id][2][i].indexOf("[b]") == -1? "" : "Tylko na pojazdy zabytkowe") +
+            (dane_id[e.id][2][i].indexOf("[c]") == -1? "" : "Nieużywane (stan na 5.03.2024)") +
+            (dane_id[e.id][2][i].indexOf("[h]") == -1? "" : "dla motocykli i pojazdów zabytkowych") +
+            (dane_id[e.id][2][i].indexOf("[j]") == -1? "" : "dla samochodów, motocykli i ciągników") +
+            (dane_id[e.id][2][i].indexOf("[j]") == -1? "" : "dla motocykli") +
+            "' class='tab " + 
+            (dane_id[e.id][2][i].indexOf("[b]") == -1? "" : " tab-zabytek") +
+            (dane_id[e.id][2][i].indexOf("[c]") == -1? "" : " tab-nieuzywane") +
+            "'>" + dane_id[e.id][2][i].replace("[a]", "").replace("[b]", "").replace("[c]", "").replace("[d]", "").replace("[g]", "").replace("[h]", "").replace("[j]", "").replace("[k]", "") + "</span> ";
+        document.getElementById(e.id).classList.add("this");
+        nowopen = e.id
+    } catch (f) {
+        e.style.fill = "black";
+    }
+}
